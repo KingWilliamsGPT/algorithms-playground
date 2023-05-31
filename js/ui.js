50b2cb1470ab630a3846bee066819f36b82c427f
KingWilliamsGPT <williamusanga22@gmail.com>
2023-05-31 05:18:10 -0700

    bare minimum

---
+++ js/ui.js
@@ -0,0 +1,25 @@
+/**
+ * UI code
+ * 
+ * This handles the swapping of the boxes
+ * 
+ * LICENCE
+ *  THIS CODE IS LICENCE UNDER MIT LICENCE. THE CODE IS FREE AND PUBLICLY AVAILABLE
+ */
+
+
+
+
+// REPRESENT CODE FUNCTIONS OR OOP
+
+
+
+class Tile{
+
+
+}
+
+
+class TileContainer{
+    
+}
